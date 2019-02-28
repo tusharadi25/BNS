b3e50c28e740f6a94169b46e0e05405bd6f1cfc8
tusharadi25 <tusharadi25@outlook.com>
2019-02-28 20:20:42 +0530

    Formatting

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-import hashlib, json, time
+import hashlib
+import json
+import os
+import sys
+import threading
+import time
 from urllib.parse import urlparse
 from uuid import uuid4
 import ipfsapi
-from flask import Flask, jsonify, request, send_from_directory, Response, send_file
-import os,sys, threading
+from flask import (Flask, Response, jsonify, request, send_file,
+                   send_from_directory)
+
 from _thread import start_new_thread
+
 
 def h(block):
     block_string = json.dumps(block, sort_keys=True).encode()
     return hashlib.sha256(block_string).hexdigest()
 
+
 class Blockchain:
     def __init__(self):
-        self.chain=[]
+        self.chain = []
         self.current_transactions = []
         self.nodes = set()
-        self.genesis=self.new_block(prev_hash='0',proof=100)
+        self.genesis = self.new_block(prev_hash='0', proof=100)
 
     def register_node(self, address):
         parsed_url = urlparse(address)
@@ -27,28 +35,23 @@ class Blockchain:
         else:
             raise ValueError('Invalid URL')
 
-
     def valid_chain(self, chain):
         last_block = chain[0]
         current_index = 1
 
         while current_index < len(chain):
             block = chain[current_index]
-            print(f'{last_block}')
-            print(f'{block}')
-            print("\n-----------\n")
             last_block_hash = h(last_block)
             if block['previous_hash'] != last_block_hash:
                 return False
             if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
                 return False
-
             last_block = block
             current_index += 1
-
         return True
+
     def resolve_conflicts(self):
-       
+
         neighbours = self.nodes
         new_chain = None
 
@@ -74,7 +77,8 @@ class Blockchain:
             return True
 
         return False
-    def new_block(self,proof,prev_hash):
+
+    def new_block(self, proof, prev_hash):
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time.time(),
@@ -86,21 +90,19 @@ class Blockchain:
         self.chain.append(block)
         return block
 
-    def new_transaction(self, sender, recipient, domain,zoneHash):
-       
+    def new_transaction(self, sender, recipient, domain, zoneHash):
+
         self.current_transactions.append({
             'senderID': sender,
             'buyerID': recipient,
             'domain': domain,
-            'zoneHash':zoneHash,
+            'zoneHash': zoneHash,
         })
-
         return self.last_block['index'] + 1
+
     @property
     def last_block(self):
         return self.chain[-1]
-
-   
 
     def proof_of_work(self, last_block):
         last_proof = last_block['proof']
@@ -117,18 +119,20 @@ class Blockchain:
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+
 app = Flask(__name__, static_folder=os.getcwd())
-p=os.getcwd()
+p = os.getcwd()
 blockchain = Blockchain()
-api=ipfsapi.connect('127.0.0.1',5001)
+api = ipfsapi.connect('127.0.0.1', 5001)
 print(api)
+
 
 @app.route('/mine', methods=['GET'])
 def mine():
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     proof = blockchain.proof_of_work(last_block)
-    ipfsid=api.id()['ID']
+    ipfsid = api.id()['ID']
     blockchain.new_transaction(
         sender="0",
         recipient=ipfsid,
@@ -153,7 +157,8 @@ def mine():
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['domain'],values['zoneHash'])
+    index = blockchain.new_transaction(
+        values['sender'], values['recipient'], values['domain'], values['zoneHash'])
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
 
@@ -170,33 +175,35 @@ def full_chain():
 @app.route('/nodes/register', methods=['GET'])
 def register_nodes():
     res = api.add('conn')
-    h=res['Hash']
+    h = res['Hash']
+
     def find():
         os.system("ipfs dht findprovs "+h+"> peers")
-    t1=threading.Thread(target=find)
+    t1 = threading.Thread(target=find)
     t1.start()
     time.sleep(10)
-    lis=[]
-    with open("peers","r") as f:
-        _=f.readline()
+    lis = []
+    with open("peers", "r") as f:
+        _ = f.readline()
         for line in f:
             line.rstrip("\n")
             print(line)
-            li=api.dht_findpeer(line[:-1])
-            li=li['Responses'][0]['Addrs']
+            li = api.dht_findpeer(line[:-1])
+            li = li['Responses'][0]['Addrs']
             for ele in li:
-                if ele.split("/")[1]== "ip4":
-                    a=ele.split("/")[2]
+                if ele.split("/")[1] == "ip4":
+                    a = ele.split("/")[2]
                     try:
-                        response = requests.get(f'http://{a}:5000/chain',timeout=1)
+                        response = request.get(
+                            f'http://{a}:5000/chain', timeout=1)
                         if response.status_code == 200:
-                            if a !="127.0.0.1":
+                            if a != "127.0.0.1":
                                 lis.append(a)
                     except:
-                        print("",end="")
+                        print("", end="")
 
     for i in lis:
-        i="http://"+i+":5000"
+        i = "http://"+i+":5000"
     nodes = lis
     print(nodes)
     if nodes is None:
@@ -229,22 +236,24 @@ def consensus():
 
     return jsonify(response), 200
 
+
 @app.route('/', methods=['GET'])
 def serve():
-    content=open("index.html").read()
+    content = open("index.html").read()
     return Response(content, mimetype="text/html")
 
 
 @app.route('/<path:path>')
-def get_resource(path):  # pragma: no cover
+def get_resource(path):
     complete_path = os.path.join(p, path)
     return send_file(complete_path)
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=5000,
+                        type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
     app.run(host='0.0.0.0', port=port)
-    
